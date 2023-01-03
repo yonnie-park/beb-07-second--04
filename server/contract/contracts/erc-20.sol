d20050c1a255b9b909676e9d9aa9eeff2e115c09
NotoriousHong <hjs97040@gmail.com>
2023-01-03 17:20:30 +0900

    add:contract folder

---
+++ server/contract/contracts/erc-20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SimpleToken is ERC20 {
+    constructor() ERC20("SimpleToken", "SIM") {
+        _mint(msg.sender, 10000);
+    }
+}
