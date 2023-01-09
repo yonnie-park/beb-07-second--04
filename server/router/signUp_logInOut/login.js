b8438271a1f13da08b248465faafe7a0235e7cdc
Beomhyuck <103881061+Beomhyuck@users.noreply.github.com>
2023-01-09 11:58:20 +0900

    edit login, web3를 이용하여 smart contract 접근

--- server/router/signUp_logInOut/login.js
+++ server/router/signUp_logInOut/login.js
@@ -6,13 +6,26 @@ const lightwallet = require("eth-lightwallet/");
 const SignerProvider = require('ethjs-provider-signer');
 const sign = require('ethjs-signer').sign;
 const Eth = require('ethjs-query');
-
+const Web3 = require('web3');
 
 const { erc20_ABI } = require('../../contract/web3js/ABI');
 
-const erc20ContractAddr = '0xe9FA229F8737f43BaBF747fBf76D821fB7Cb9a1A'; //ganache erc20 CA
+const web3 = new Web3(new Web3.providers.HttpProvider('https://goerli.infura.io/v3/0554b32c0e7245e1b1a36bd726defb9d')); //ganache provider
 
+const erc20ContractAddr = '0xe9FA229F8737f43BaBF747fBf76D821fB7Cb9a1A'; //ganache erc20 CA
+const erc20Contract = new web3.eth.Contract(erc20_ABI, erc20ContractAddr); //erc20 contract 인스턴스화
 // const db = {user_id:"1", user_password:"2", user_nickname:"3", user_loginTime:"2023-01-03"};
+async function getAccount() {
+    try {
+        const accounts = await web3.eth.getAccounts();
+
+        console.log(accounts);
+        return accounts;
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+}
 
 router.post('/', async(req, res)=>{
     const {user_id, user_password} = req.body;
@@ -25,33 +38,35 @@ router.post('/', async(req, res)=>{
     const month = timeSplit[1];
     const day = timeSplit[2];
 
-    let server_provider;
-    let server_privateKey;
+    // let server_provider;
+    // let server_privateKey;
 
-    let erc20Contract = new web3.eth.Contract(erc20_ABI, erc20ContractAddr); //erc20 contract 인스턴스화
+    getAccount().then(console.log);
+    balance = web3.eth.getBalance('0x23802188302BDFc1fdA5246211698227873D7Db2').then(e=>{console.log(e)});
     
-    db.query('SELECT * FROM user WHERE user_id = \'server\'', function(err,results){
-        console.log(results);
-        const keystore = lightwallet.keystore.deserialize(results[0].user_keystore);
-        const address = keystore.getAddresses();
 
-        console.log(keystore);
-        console.log(address);
+    // db.query('SELECT * FROM user WHERE user_id = \'server\'', function(err,results){
+    //     console.log(results);
+    //     const keystore = lightwallet.keystore.deserialize(results[0].user_keystore);
+    //     const address = keystore.getAddresses();
+
+    //     console.log(keystore);
+    //     console.log(address);
         
         
-        keystore.keyFromPassword(results[0].user_password, (err, data) => {
+    //     keystore.keyFromPassword(results[0].user_password, (err, data) => {
 
-            const key = keystore.exportPrivateKey(address.toString(), data);
+    //         const key = keystore.exportPrivateKey(address.toString(), data);
     
-            server_privateKey = '0x' + key;
-        });
+    //         server_privateKey = '0x' + key;
+    //     });
 
-        server_provider = new SignerProvider('https://goerli.infura.io/v3/0554b32c0e7245e1b1a36bd726defb9d', {
-            signTransaction: (rawTx, cb) => cb(null, sign(rawTx, server_privateKey.toString())),
-            accounts: (cb) => cb(null, [address.toString()]),
-        });
+    //     server_provider = new SignerProvider('https://goerli.infura.io/v3/0554b32c0e7245e1b1a36bd726defb9d', {
+    //         signTransaction: (rawTx, cb) => cb(null, sign(rawTx, server_privateKey.toString())),
+    //         accounts: (cb) => cb(null, [address.toString()]),
+    //     });
 
-    });
+    // });
 
 
     db.query('SELECT * FROM user WHERE user_id =? AND user_password =?', 
@@ -63,38 +78,36 @@ router.post('/', async(req, res)=>{
             req.session.user_id = user_id;
             req.session.user_nickname = results[0].user_nickname;
 
-            console.log(req.session);
-            console.log(results);
+            // console.log(req.session);
+            // console.log(results);
             let lastLoginTime = results[0].user_loginTime;
             // console.log(lastLoginTime);
             if(!lastLoginTime){
                 lastLoginTime = '0000-00-00';
             }
             const dbTime = lastLoginTime.split('-');
-            console.log(dbTime);
+            // console.log(dbTime);
 
-            const keystore = lightwallet.keystore.deserialize(results[0].user_keystore);
-            const address = keystore.getAddresses();
+            // const keystore = lightwallet.keystore.deserialize(results[0].user_keystore);
+            // const address = keystore.getAddresses();
 
-            console.log(keystore);
-            console.log(address);
-            let privateKey;
+            // console.log(keystore);
+            // console.log(address);
+            // let privateKey;
 
-            keystore.keyFromPassword(results[0].user_password, (err, data) => {
+            // keystore.keyFromPassword(results[0].user_password, (err, data) => {
 
-                const key = keystore.exportPrivateKey(address.toString(), data)
+            //     const key = keystore.exportPrivateKey(address.toString(), data)
         
-                privateKey = '0x' + key
-                console.log(privateKey);
-            });
+            //     privateKey = '0x' + key
+            //     console.log(privateKey);
+            // });
 
-            const provider = new SignerProvider('https://goerli.infura.io/v3/0554b32c0e7245e1b1a36bd726defb9d', {
-                signTransaction: (rawTx, cb) => cb(null, sign(rawTx, privateKey.toString())),
-                accounts: (cb) => cb(null, [address.toString()]),
-            });
+            // const provider = new SignerProvider('https://goerli.infura.io/v3/0554b32c0e7245e1b1a36bd726defb9d', {
+            //     signTransaction: (rawTx, cb) => cb(null, sign(rawTx, privateKey.toString())),
+            //     accounts: (cb) => cb(null, [address.toString()]),
+            // });
 
-
-            console.log(provider);
             if (dbTime[0] != year && dbTime[1] != month && dbTime[2] != day){
                 //첫번쨰 로그인
                 // token 전송기능
