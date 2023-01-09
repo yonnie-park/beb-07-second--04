ac3888d94e9d4efdffc9997ed8cad3761caabf97
송해현 <jea752@daum.net>
2023-01-09 15:07:44 +0900

    Add:db-tweet

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,9 @@ import React, {useState} from "react";
 import { Link} from "react-router-dom";
 import "./Header.css"
 
+
+
+
 export default function Header(props){
 
     const [account, setAccount]=useState("")
@@ -9,11 +12,15 @@ export default function Header(props){
     const [isConnected, setIsConnected]=useState(false)
 
 
+
+
     return(
         <div className="header">
                 <div className="logoDiv"><Link to="/" onClick="location.reload();" className="mainlogo">SNORLAX</Link></div>
                 <div className="mypageDiv"><Link to="/mypage" className="menu">my page</Link></div>
+                
                 <div className="loginDiv"><Link to="/login"><button className="loginBTN">Login</button></Link></div>
+                    
                 <div className="signupDiv"><Link to="/signup"><button className="joinBTN">Sign up</button></Link></div>
         </div>
     )
