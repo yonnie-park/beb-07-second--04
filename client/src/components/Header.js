14fed77e6b8c31d9d8b61f49521e23ae3d1915c3
yonnie-park <juon0805@gmail.com>
2023-01-09 15:21:08 +0900

    edit: logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,20 +8,20 @@ export default function Header(props){
     const [isConnected, setIsConnected]=useState(false)
 
     function handleConnection(){
-        if (props){
+        if (props.length>0){
             setIsConnected(true)
             setAccount(props.user_id)
         }
     }
     function handleLogout(){
         axios.get("http://localhost:8080/logout")
-        .then(console.log("logout success"))
+        .then(setIsConnected(false))
     }
     return(
         <div className="header">
                 <div className="logoDiv"><Link to="/" onClick="location.reload();" className="mainlogo">SNORLAX</Link></div>
                 <div className="mypageDiv"><Link to="/mypage/:account" className="menu" account={account}>my page</Link></div>
-                <div className="loginDiv"> {isConnected? <Link to="/logout"><button className="loginBTN" onClick={handleLogout()}>Logout</button></Link> : <Link to="/login"><button className="loginBTN" onClick={handleConnection()}>Login</button></Link>}</div>
+                <div className="loginDiv"> {isConnected? <Link to="/"><button className="loginBTN" onClick={handleLogout()}>Logout</button></Link> : <Link to="/login"><button className="loginBTN" onClick={handleConnection()}>Login</button></Link>}</div>
                 {isConnected? console.log("connected"):<div className="signupDiv"><Link to="/signup"><button className="joinBTN">Sign up</button></Link></div>}
         </div>
     )
