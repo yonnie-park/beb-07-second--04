5d7acc84afe7977308c5a299c833c2c32af59acf
yonnie-park <juon0805@gmail.com>
2023-01-10 14:44:51 +0900

    add: usecontext

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,24 @@ import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import UseContext from "./UseContext"
 function App() {
   const [account, setAccount] = useState("");
-
+  
   return (
     <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path ="/" element={<Main/>}/>
-        <Route path="/mypage/:account" element={<Mypage account={account}/>}/>
-        <Route path="/mint" element={<Mint/>}/>
-        <Route path="/view" element={<View/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-      </Routes>
-      <Footer/>
+      <UseContext.Provider value={account}>
+        <Header/>
+        <Routes>
+          <Route path ="/" element={<Main/>}/>
+          <Route path="/mypage/:account" element={<Mypage account={account}/>}/>
+          <Route path="/mint" element={<Mint/>}/>
+          <Route path="/view" element={<View/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+        <Footer/>
+      </UseContext.Provider>
     </BrowserRouter>
   );
 }
